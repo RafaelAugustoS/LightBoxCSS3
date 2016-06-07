7e1e102e84a66bfc5bc8b87c128fff44d627482d
Rafael Augusto <rafa.2098@gmail.com>
2016-06-06 22:11:25 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>LightBox com CSS3</title>
+	<link rel="stylesheet" href="estilo.css">
+	<link href="http://fonts.googleapis.com/css?family=Raleway:200,100,400" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<h1>Puro CSS Lightbox</h1>
+
+	<!-- Thumbnail -->
+	<a href="#img1">
+		<img src="steve.jpg">
+	</a>
+	
+	<!-- Lightbox escondido -->
+	<a href="#_" class="lightbox" id="img1">
+		<img src="steve.jpg">
+	</a>	
+</body>
+</html>
